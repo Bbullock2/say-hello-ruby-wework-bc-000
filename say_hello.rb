e788fa807fa12a72de7d3b6fa7014c07919a5aa3
Mary Bridget Bullock <bridget.bullock@wework.com>
2018-02-09 00:40:41 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,17 @@
 
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
+=======
+def say_hello(name)
+  name = Ruby Programer
+  puts "Hello #{name}!"
+end
+
+name = Kent Beck
+
+say_hello(name)
+
+say_hello
+>>>>>>> 821c55323b78c8efdd8adf3cae0eaac678dd2242
